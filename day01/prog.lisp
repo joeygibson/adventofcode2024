8bf230cf3da37f043969a80bc1f03d6bb175bf0a
Joey Gibson <joey@joeygibson.com>
2024-12-01 00:10:53 -0500

    day 1, part 1

---
+++ day01/prog.lisp
@@ -0,0 +1,37 @@
+(ql:quickload :cl-ppcre)
+(ql:quickload :split-sequence)
+(ql:quickload :lisp-utils)
+(ql:quickload :alexandria)
+
+; (ql:quickload :queues)
+
+; (require :queues.simple-queue)
+
+
+(use-package :lisp-utils)
+
+(defun parse (lines)
+  (let ((list0 nil)
+        (list1 nil))
+    (dolist (line lines)
+      (let ((chunks (cl-ppcre:split "\\s+" line)))
+        (push (parse-integer (first chunks)) list0)
+        (push (parse-integer (second chunks)) list1)))
+    (list (sort list0 #'<)
+          (sort list1 #'<))))
+
+(defun compute-diffs (list0 list1)
+  (loop for i0 in list0
+        for i1 in list1
+        collecting (abs (- i0 i1))))
+
+
+(defun part1 (file-name)
+  (let* ((lines (uiop:read-file-lines file-name)))
+    (destructuring-bind (list0 list1) (parse lines)
+      (apply #'+ (compute-diffs list0 list1)))))
+
+(part1 "input0.txt")
+(part1 "input1.txt")
+
+
