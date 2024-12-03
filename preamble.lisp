762745e29201968d664805c0ae9223ece622aee1
Joey Gibson <joey@joeygibson.com>
2024-12-02 23:30:41 -0500

    updated preamble.lisp

--- preamble.lisp
+++ preamble.lisp
@@ -10,5 +10,20 @@
 
 (use-package :lisp-utils)
 
+(defun parse (file-name)
+  (let* ((lines (uiop:read-file-lines file-name)))))
+
+(defun part1 (file-name)
+  (let* ((data (parse file-name)))))
+
+(defun part2 (file-name)
+  (let* ((data (parse file-name)))))
+
+(print (part1 "input0.txt"))
+; (print (part1 "input1.txt"))
+
+; (print (part2 "input0.txt"))
+; (print (part2 "input1.txt"))
+
 
 
