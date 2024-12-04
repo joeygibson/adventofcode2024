36268d2a69d624e03f7fe5db3e8b05eae2baa6f2
Joey Gibson <joey@joeygibson.com>
2024-12-04 01:38:38 -0500

    day 4, part 2

--- day04/prog.lisp
+++ day04/prog.lisp
@@ -20,10 +20,10 @@
                    do (setf (gethash (cons i j) puzzle) ch)))
     (values puzzle (length (first lines)) (length lines))))
 
-(defun find-all-xs (puzzle)
+(defun find-all-starts (puzzle letter)
   (let ((xs nil))
     (maphash (lambda (k v)
-               (when (equal v "X")
+               (when (equal v letter)
                  (push k xs)))
              puzzle)
     (nreverse xs)))
@@ -70,37 +70,93 @@
             neighbors))
     neighbors))
 
-(defun xmas-p (puzzle coords)
+(defstruct x-mas top-left top-right bottom-left bottom-right)
+
+(defun get-neighbors-part-2 (spot width height)
+  (let* ((neighbors nil)
+         (x (car spot))
+         (y (cdr spot))
+         (up-room (>= (- y 1) 0))
+         (down-room (< (+ y 1) height))
+         (left-room (>= (- x 1) 0))
+         (right-room (< (+ x 1) width))
+         (possible-top-left-corner (cons (- x 1) (- y 1)))
+         (possible-top-right-corner (cons (+ x 1) (- y 1)))
+         (possible-bottom-left-corner (cons (- x 1) (+ y 1)))
+         (possible-bottom-right-corner (cons (+ x 1) (+ y 1))))
+    (when (and up-room down-room left-room right-room)
+      (push (make-x-mas :top-left possible-top-left-corner
+                        :top-right possible-top-right-corner
+                        :bottom-left possible-bottom-left-corner
+                        :bottom-right possible-bottom-right-corner)
+            neighbors))
+    neighbors))
+
+(defun xmas-p (puzzle coords letters)
   (let ((matches 0))
-    (loop for l in '("X" "M" "A" "S")
+    (loop for l in letters
           for spot in coords
           do (when (equal (gethash spot puzzle) l)
                (incf matches)))
-    (eq matches 4)))
+    (eq matches (length letters))))
 
 (defun part1 (file-name)
   (multiple-value-bind (puzzle width height) (parse file-name)
-    (let* ((xs (find-all-xs puzzle))
+    (let* ((xs (find-all-starts puzzle "X"))
            (matches nil))
       (dolist (spot xs)
         (let ((neighbors (get-neighbors spot width height)))
           (loop for neighbor in neighbors
-                if (xmas-p puzzle neighbor)
+                if (xmas-p puzzle neighbor '("X" "M" "A" "S"))
                   do (push neighbor matches))))
       (length matches))))
 
+
+
+(defun check-xmas (puzzle xmas)
+  (let* ((top-left (gethash (x-mas-top-left xmas) puzzle))
+         (top-right (gethash (x-mas-top-right xmas) puzzle))
+         (bottom-left (gethash (x-mas-bottom-left xmas) puzzle))
+         (bottom-right (gethash (x-mas-bottom-right xmas) puzzle)))
+    (cond ((and (and (equal top-left "M")
+                     (equal bottom-right "S"))
+                (and (equal top-right "M")
+                     (equal bottom-left "S")))
+           t)
+          ((and (and (equal top-left "S")
+                     (equal bottom-right "M"))
+                (and (equal top-right "S")
+                     (equal bottom-left "M")))
+           t)
+          ((and (and (equal top-left "M")
+                     (equal bottom-right "S"))
+                (and (equal top-right "S")
+                     (equal bottom-left "M")))
+           t)
+          ((and (and (equal top-left "S")
+                     (equal bottom-right "M"))
+                (and (equal top-right "M")
+                     (equal bottom-left "S")))
+           t)
+          (t nil))))
+
 (defun part2 (file-name)
-  (let* ((data (parse file-name)))))
+  (multiple-value-bind (puzzle width height) (parse file-name)
+    (let* ((xs (find-all-starts puzzle "A"))
+           (matches nil))
+      (dolist (spot xs)
+        (let ((neighbors (get-neighbors-part-2 spot width height)))
+          (loop for neighbor in neighbors
+                if (check-xmas puzzle neighbor)
+                  do (push neighbor matches))))
+      (length matches))))
 
 (print (part1 "input0.txt"))
 (print (part1 "input1.txt"))
 
-; (print (part2 "input0.txt"))
-; (print (part2 "input1.txt"))
+(print (part2 "input0.txt"))
+(print (part2 "input1.txt"))
 
-(multiple-value-bind (puzzle width height) (parse "input0.txt")
-  (let* ((xs (find-all-xs puzzle))
-         (neighbors (get-neighbors (first xs) width height)))
-    (print (get-neighbors (cons 5 0) width height))))
+
 
 
