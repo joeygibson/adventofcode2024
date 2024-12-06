e76d067721c2f6437e13e40917190b7c7beba107
Joey Gibson <joey@joeygibson.com>
2024-12-06 10:56:58 -0500

    day 6 - further clean up

--- day06/prog.lisp
+++ day06/prog.lisp
@@ -18,15 +18,13 @@
             (guard-x self)
             (guard-y self))))
 
-(defclass furniture ()
-  ())
+(defclass furniture () ())
 
 (defclass floor-tile ()
   ((visited :initform nil :accessor visited)
    (visit-count :initform 0 :accessor visit-count)))
 
-(defclass obstacle ()
-  ())
+(defclass obstacle () ())
 
 (defmethod add-visit ((self floor-tile))
   (incf (visit-count self)))
@@ -37,11 +35,12 @@
         ((equal chr ".")
          (make-instance 'floor-tile))
         ((equal chr "#")
-         (make-instance 'furniture))))
+         (make-instance 'furniture))
+        ((equal chr "O")
+         (make-instance 'obstacle))))
 
-(defun parse (file-name)
-  (let* ((lines (uiop:read-file-lines file-name))
-         (the-map (make-hash-table :test #'equal))
+(defun create-map-and-guard (lines)
+  (let* ((the-map (make-hash-table :test #'equal))
          (guard nil))
     (loop for row in lines
           for r from 0
@@ -56,38 +55,41 @@
                         (setf (gethash (cons c r) the-map) obj))))
     (values the-map guard)))
 
+(defun parse (file-name)
+  (let* ((lines (uiop:read-file-lines file-name)))
+    (multiple-value-bind (the-map guard) (create-map-and-guard lines)
+      (values the-map guard lines))))
+
 (defun play (the-map guard)
-  (let ((in-bounds t))
-    (loop while in-bounds
-          do (let* ((guard-direction (direction guard))
-                    (old-x (guard-x guard))
-                    (old-y (guard-y guard))
-                    (x (cond ((eql (direction guard) :left)
-                              (1- old-x))
-                             ((eql (direction guard) :right)
-                              (1+ old-x))
-                             (t old-x)))
-                    (y (cond ((eql (direction guard) :up)
-                              (1- old-y))
-                             ((eql (direction guard) :down)
-                              (1+ old-y))
-                             (t old-y))))
-               (let ((next-spot (gethash (cons x y) the-map)))
-                 (cond ((null next-spot) ; out of bounds
-                        (setf in-bounds nil))
-                       ((typep next-spot 'floor-tile) ; floor-tile
-                        (when (> (visit-count next-spot) 5)
-                          (return 'in-a-loop))
-                        (setf (visited next-spot) t)
-                        (add-visit next-spot)
-                        (setf (guard-x guard) x)
-                        (setf (guard-y guard) y))
-                       (t
-                        (let ((new-direction (cond ((eql guard-direction :up) :right)
-                                                   ((eql guard-direction :right) :down)
-                                                   ((eql guard-direction :down) :left)
-                                                   ((eql guard-direction :left) :up))))
-                          (setf (direction guard) new-direction)))))))))
+  (loop do (let* ((guard-direction (direction guard))
+                  (old-x (guard-x guard))
+                  (old-y (guard-y guard))
+                  (x (cond ((eql (direction guard) :left)
+                            (1- old-x))
+                           ((eql (direction guard) :right)
+                            (1+ old-x))
+                           (t old-x)))
+                  (y (cond ((eql (direction guard) :up)
+                            (1- old-y))
+                           ((eql (direction guard) :down)
+                            (1+ old-y))
+                           (t old-y))))
+             (let ((next-spot (gethash (cons x y) the-map)))
+               (cond ((null next-spot) ; out of bounds
+                      (return 'out-of-bounds))
+                     ((typep next-spot 'floor-tile) ; floor-tile
+                      (when (> (visit-count next-spot) 5)
+                        (return 'in-a-loop))
+                      (setf (visited next-spot) t)
+                      (add-visit next-spot)
+                      (setf (guard-x guard) x)
+                      (setf (guard-y guard) y))
+                     (t
+                      (let ((new-direction (cond ((eql guard-direction :up) :right)
+                                                 ((eql guard-direction :right) :down)
+                                                 ((eql guard-direction :down) :left)
+                                                 ((eql guard-direction :left) :up))))
+                        (setf (direction guard) new-direction))))))))
 
 (defun part1 (file-name)
   (multiple-value-bind (the-map guard) (parse file-name)
@@ -97,16 +99,16 @@
                      (visited spot)))))
 
 (defun part2 (file-name)
-  (multiple-value-bind (the-map guard) (parse file-name)
+  (multiple-value-bind (the-map guard lines) (parse file-name)
     (let* ((guard-pos (cons (guard-x guard)
                             (guard-y guard)))
            (loop-count 0))
       (loop for pos being the hash-keys of the-map
             do (progn
-                 (multiple-value-bind (map-copy guard) (parse file-name)
+                 (multiple-value-bind (map-copy guard) (create-map-and-guard lines)
                    (when (and (not (equal pos guard-pos))
                               (typep (gethash pos the-map) 'floor-tile))
-                     (setf (gethash pos map-copy) (make-instance 'obstacle))
+                     (setf (gethash pos map-copy) (create-object "O"))
                      (let ((results (play map-copy guard)))
                        (when (equal results 'in-a-loop)
                          (incf loop-count)))))))
@@ -117,4 +119,5 @@
 
 (print (part2 "input0.txt"))
 (print (part2 "input1.txt"))
+
 
