3aad266b691537faeeab5855f79cf71de20afafe
Joey Gibson <joey@joeygibson.com>
2024-12-06 01:39:53 -0500

    day 6, part 2 - partial

--- day06/prog.lisp
+++ day06/prog.lisp
@@ -22,7 +22,14 @@
   ())
 
 (defclass floor-tile ()
-  ((visited :initform nil :accessor visited)))
+  ((visited :initform nil :accessor visited)
+   (visit-count :initform 0 :accessor visit-count)))
+
+(defclass obstacle ()
+  ())
+
+(defmethod add-visit ((self floor-tile))
+  (incf (visit-count self)))
 
 (defun create-object (chr)
   (cond ((equal chr "^")
@@ -66,17 +73,24 @@
                               (1+ old-y))
                              (t old-y))))
                (let ((next-spot (gethash (cons x y) the-map)))
-                 (cond ((null next-spot)
+                 (cond ((null next-spot) ; out of bounds
                         (setf in-bounds nil))
-                       ((typep next-spot 'floor-tile)
+                       ((typep next-spot 'floor-tile) ; floor-tile
+                        (print (visit-count next-spot))
+                        (when (> (visit-count next-spot) 1)
+                          (print "LOOP")
+                          (return 'in-a-loop))
                         (setf (visited next-spot) t)
+                        (add-visit next-spot)
                         (setf (guard-x guard) x)
                         (setf (guard-y guard) y))
-                       (t (let ((new-direction (cond ((eql guard-direction :up) :right)
-                                                     ((eql guard-direction :right) :down)
-                                                     ((eql guard-direction :down) :left)
-                                                     ((eql guard-direction :left) :up))))
-                            (setf (direction guard) new-direction)))))))))
+                       ((or (typep next-spot 'furniture)
+                            (typep next-spot 'obstacle))
+                        (let ((new-direction (cond ((eql guard-direction :up) :right)
+                                                   ((eql guard-direction :right) :down)
+                                                   ((eql guard-direction :down) :left)
+                                                   ((eql guard-direction :left) :up))))
+                          (setf (direction guard) new-direction)))))))))
 
 
 
@@ -88,12 +102,24 @@
                      (visited spot)))))
 
 (defun part2 (file-name)
-  (let* ((data (parse file-name)))))
+  (multiple-value-bind (the-map guard) (parse file-name)
+    (let* ((guard-pos (cons (guard-x guard)
+                            (guard-y guard)))
+           (loop-count 0))
+      (loop for pos being the hash-key of the-map
+            do (progn
+                 (when (and (not (equal pos guard-pos))
+                            (typep (gethash pos the-map) 'floor-tile))
+                   (let* ((map-copy (alexandria:copy-hash-table the-map)))
+                     (setf (gethash pos map-copy) (make-instance 'obstacle))
+                     (when (equal (play map-copy guard) 'in-a-loop)
+                       (incf loop-count))))))
+      loop-count)))
 
 (print (part1 "input0.txt"))
 (print (part1 "input1.txt"))
 
-; (print (part2 "input0.txt"))
+(print (part2 "input0.txt"))
 ; (print (part2 "input1.txt"))
 
 
