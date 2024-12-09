27c605d1c1f7fdf3c06e2c107da9a00e19981f1a
Joey Gibson <joey@joeygibson.com>
2024-12-09 09:54:31 -0500

    day 9, part 2 - works on sample

--- day09/prog.lisp
+++ day09/prog.lisp
@@ -41,21 +41,65 @@
                     for chunk-index downfrom (1- (length map))
                     when (not (equal chunk "."))
                       do (progn
-                           ;(break)
                            (let ((empty-index (position "." map :test #'equal)))
                              (when (and empty-index
                                         (< empty-index chunk-index))
                                (rotatef (nth chunk-index map) (nth empty-index map))))))))
     map))
 
-(defun get-file-chunk (map start-pos)
-  (let* ((id (nth start-pos map))
-         (file (take-while (drop map start-pos) (lambda (chr)
-                                                  (equal chr id)))))
-    file))
+(defun find-chunks (map)
+  (let* ((chunks nil)
+         (current-val (first map))
+         (current-chunk-start 0)
+         (current-chunk (list current-val)))
+    (loop for i from 1 to (length map)
+          do (let ((chr (nth i map)))
+               (if (equal chr current-val)
+                   (push chr current-chunk)
+                   (progn
+                     (push (cons (reverse current-chunk)
+                                 (cons current-chunk-start (1- i)))
+                           chunks)
+                     (setf current-val chr)
+                     (setf current-chunk (list chr))
+                     (setf current-chunk-start i)))))
+    (reverse chunks)))
 
-(defun get-file-chunks (map)
-  (let (())))
+(defun chunk-len (start end)
+  (1+ (- end start)))
+
+(defun compact-2 (map)
+  (let* ((chunks (find-chunks map))
+         (file-chunks (reverse (remove-if-not (lambda (chunk)
+                                                     (not (equal (caar chunk) ".")))
+                                              chunks)))
+         (empty-chunks (remove-if-not (lambda (chunk)
+                                        (equal (caar chunk) "."))
+                                      chunks)))
+    (loop for file-chunk in file-chunks
+          do (let* ((chunk-start (cadr file-chunk))
+                    (chunk-end (cddr file-chunk))
+                    (chunk-length (chunk-len chunk-start chunk-end))
+                    (available-empty-chunk (loop for empty-chunk in empty-chunks
+                                                 if (>= (chunk-len (cadr empty-chunk)
+                                                                   (cddr empty-chunk))
+                                                        chunk-length)
+                                                   return empty-chunk)))
+               (when available-empty-chunk
+                 (let* ((empty-start (cadr available-empty-chunk))
+                        (empty-end (cddr available-empty-chunk))
+                        (empty-length (chunk-len empty-start empty-end))
+                        (shorter-empty-end (+ empty-start (1- chunk-length))))
+                   (rotatef (subseq map chunk-start (1+ chunk-end))
+                            (subseq map empty-start (+ empty-start chunk-length)))
+                   (if (= chunk-length empty-length)
+                       (setf empty-chunks (remove available-empty-chunk empty-chunks))
+                       (let* ((empty-pos (position available-empty-chunk empty-chunks))
+                              (new-start (+ empty-start chunk-length))
+                              (new-empty-chunk (cons (cadr available-empty-chunk)
+                                                     (cons new-start (cddr available-empty-chunk)))))
+                         (setf (nth empty-pos empty-chunks) new-empty-chunk)))))))
+    map))
 
 (defun part1 (file-name)
   (let* ((disk-map (parse file-name))
@@ -71,14 +115,13 @@
     (loop for chunk in compact-map
           for i from 0
           if (not (equal chunk "."))
-          summing (* (parse-integer chunk) i))))
-
+            summing (* (parse-integer chunk) i))))
 
 (print (part1 "input0.txt"))
 (print (part1 "input1.txt"))
 
-; (print (part2 "input0.txt"))
-; (print (part2 "input1.txt"))
+(print (part2 "input0.txt"))
+(print (part2 "input1.txt"))
 
 
 
