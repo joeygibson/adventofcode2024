efdffcc38568a290260a5640b93be35e51647a50
Joey Gibson <joey@joeygibson.com>
2024-12-10 23:46:03 -0500

    added parse-to-grid function to preamble.lisp

--- preamble.lisp
+++ preamble.lisp
@@ -10,6 +10,16 @@
 
 (use-package :lisp-utils)
 
+(defun parse-to-grid (file-name &optional (fn #'identity))
+  (let* ((lines (uiop:read-file-lines file-name))
+         (grid (make-hash-table :test #'equal)))
+    (loop for row in lines
+          for r from 0
+          do (loop for col in (cl-ppcre:split "" row)
+                   for c from 0
+                   do (setf (gethash (cons r c) grid) (funcall fn col))))
+    grid))
+
 (defun parse (file-name)
   (let* ((lines (uiop:read-file-lines file-name)))))
 
