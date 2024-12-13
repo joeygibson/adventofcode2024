b6bff2010ba1a2097e3bde71e21a698973f4840e
Joey Gibson <joey@joeygibson.com>
2024-12-13 01:38:28 -0500

    day 13, part 2

--- day13/ff.py
+++ day13/ff.py
@@ -1,28 +1,57 @@
-def find_button_presses(a_x, a_y, b_x, b_y, target_x, target_y):
-    # Initialize a large number for iterations to avoid infinite loops
-    max_iterations = 1000
-
-    # Iterate over possible values for n_A and n_B
-    for n_A in range(max_iterations):
-        for n_B in range(max_iterations):
-            # Calculate the resulting position
-            current_x = n_A * a_x + n_B * b_x
-            current_y = n_A * a_y + n_B * b_y
-
-            # Check if the current position matches the target
-            if current_x == target_x and current_y == target_y:
-                return (n_A, n_B)
-
-    return None  # Return None if no solution is found within the iteration limit
-
-# Example usage
-a_x, a_y = 94, 34  # Movement in X and Y for button A
-b_x, b_y = 22, 67  # Movement in X and Y for button B
-target_x, target_y = 8400, 5400  # Target position
-
-solution = find_button_presses(a_x, a_y, b_x, b_y, target_x, target_y)
-if solution:
-    print("Number of presses for A and B:", solution)
-else:
-    print("No solution found within the iteration limit.")
-
+# Input: lines like
+#          Button A: X+#, Y+#
+#          Button B: X+#, Y+#
+#          Prize: X=#, Y=# (add 10 trillion to each)
+#        separated by blank lines
+# Output: if it costs 3 tokens to push A and 1 to push B,
+#         what's the cheapest way to get from 0, 0 to prize location?
+#         total cost across all sets of three lines
+ 
+def get_cheapest_cost(ax, ay, bx, by, px, py):
+  # ax*na + bx*nb = px
+  # ay*na + by*nb = py
+  # thus
+  # ay*ax*na + ay*bx*nb = ay*px
+  # ax*ay*na + ax*by*nb = ax*py
+  # thus
+  # (ay*bx - ax*by)*nb = ay*px - ax*py
+  nb = int((ay*px - ax*py) / (ay*bx - ax*by))
+  na = int((px - nb*bx) / ax)
+  if (na * ax) + (nb * bx) == px and (na * ay) + (nb * by) == py:
+    return 3 * na + nb
+  return -1
+ 
+button_a = []
+button_b = []
+prize = []
+ 
+file = open("input1.txt", "r")
+line_count = 0
+for line in file:
+  line = line.replace("\n", "")
+  line_count += 1
+  if line_count % 4 == 1:
+    line = line.replace("Button A: X+", "")
+    line = line.replace(", Y+", " ")
+    parts = line.split(" ")
+    button_a.append([int(parts[0]), int(parts[1])])
+    continue
+  if line_count % 4 == 2:
+    line = line.replace("Button B: X+", "")
+    line = line.replace(", Y+", " ")
+    parts = line.split(" ")
+    button_b.append([int(parts[0]), int(parts[1])])
+    continue
+  if line_count % 4 == 3:
+    line = line.replace("Prize: X=", "")
+    line = line.replace(", Y=", " ")
+    parts = line.split(" ")
+    prize.append([int(parts[0]) + 10000000000000, int(parts[1]) + 10000000000000])
+    continue
+ 
+total = 0
+for index in range(len(button_a)):
+  cost = get_cheapest_cost(button_a[index][0], button_a[index][1], button_b[index][0], button_b[index][1], prize[index][0], prize[index][1])
+  if cost >= 0:
+    total += cost
+print (total)
--- day13/prog.lisp
+++ day13/prog.lisp
@@ -8,6 +8,9 @@
 
 (use-package :lisp-utils)
 
+(defconstant *a-cost* 3)
+(defconstant *b-cost* 1)
+
 (defclass claw ()
   ((a-x :initarg :a-x :reader a-x :type integer)
    (a-y :initarg :a-y :reader a-y :type integer)
@@ -15,12 +18,6 @@
    (b-y :initarg :b-y :reader b-y :type integer)
    (prize-x :initarg :prize-x :reader prize-x :type integer)
    (prize-y :initarg :prize-y :reader prize-y :type integer)))
-
-;; (defmethod print-object ((self claw) stream)
-;;   (print-unreadable-object (self stream)
-;;     (with-slots (a-x a-y b-x b-y prize-x prize-y) self
-;;       (format stream "~s A(~a, ~a), B(~a, ~a), Prize(~a, ~a)"
-;;               (type-of self) a-x a-y b-x b-y prize-x prize-y))))
 
 (defmethod print-object ((self claw) stream)
   (print-unreadable-object (self stream)
@@ -30,21 +27,25 @@
               (prize-x self) (prize-y self))))
 
 
-(defun make-claw (a b prize)
-  (make-instance 'claw :a-x (parse-integer (car a))
-                       :a-y (parse-integer (cadr a))
-                       :b-x (parse-integer (car b))
-                       :b-y (parse-integer (cadr b))
-                       :prize-x (parse-integer (car prize))
-                       :prize-y (parse-integer (cadr prize))))
+(defun make-claw (a b prize multiplier)
+  (let ((p-x (parse-integer (car prize)))
+        (p-y (parse-integer (cadr prize))))
+    (make-instance 'claw :a-x (parse-integer (car a))
+                               :a-y (parse-integer (cadr a))
+                               :b-x (parse-integer (car b))
+                               :b-y (parse-integer (cadr b))
+                               :prize-x (+ p-x multiplier)
+                               :prize-y (+ p-y multiplier))))
 
-(defun create-claw-machines (file-name)
+(defun create-claw-machines (file-name &key is-part-2)
   (let* ((specs (split-file-into-sections file-name)))
     (mapcar (lambda (section)
               (let* ((a (cl-ppcre:all-matches-as-strings "(\\d+)" (first section)))
                      (b (cl-ppcre:all-matches-as-strings "(\\d+)" (second section)))
                      (prize (cl-ppcre:all-matches-as-strings "(\\d+)" (third section))))
-                (make-claw a b prize)))
+                (make-claw a b prize (if is-part-2
+                                         10000000000000
+                                         0))))
             specs)))
 
 (defmethod determine-button-presses ((self claw) max-iterations)
@@ -62,8 +63,18 @@
                                      (return-from outer (list n-a n-b))))))))
       res)))
 
-(defconstant *a-cost* 3)
-(defconstant *b-cost* 1)
+; this function borrowed from https://pastebin.com/TRW9CyX7
+(defmethod determine-button-presses-huge ((self claw))
+  (with-slots (a-x a-y b-x b-y prize-x prize-y) self
+    (let* ((n-b (truncate (- (* a-y prize-x)
+                             (* a-x prize-y))
+                          (- (* a-y b-x)
+                             (* a-x b-y))))
+           (n-a (truncate (- prize-x (* n-b b-x)) a-x)))
+      (if (and (= (+ (* n-a a-x) (* n-b b-x)) prize-x)
+               (= (+ (* n-a a-y) (* n-b b-y)) prize-y))
+          (+ (* 3 n-a) n-b)
+          0))))
 
 (defun compute-cost (result)
   (let* ((a (car result))
@@ -81,13 +92,19 @@
     (reduce #'+ (mapcar #'compute-cost winners))))
 
 (defun part2 (file-name)
-  (let* ((data (parse file-name)))))
+  (let* ((machines (create-claw-machines file-name :is-part-2 t))
+         (winners nil))
+    (dolist (machine machines)
+      (let ((result (determine-button-presses-huge machine)))
+        (when result
+          (push result winners))))
+    (reduce #'+ winners)))
 
 (time (print (part1 "input0.txt")))
 (time (print (part1 "input1.txt")))
 
-; (time (print (part2 "input0.txt")))
-; (time (print (part2 "input1.txt")))
+(time (print (part2 "input0.txt")))
+(time (print (part2 "input1.txt")))
 
 
 
