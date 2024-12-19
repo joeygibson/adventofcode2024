ac56d0320c918e0ac1b27128352d026aff4a75c0
Joey Gibson <joey@joeygibson.com>
2024-12-19 00:51:08 -0500

    day 19, part 2

--- day19/prog.lisp
+++ day19/prog.lisp
@@ -17,7 +17,6 @@
   (if (= i (length design))
       1
       (let ((res 0))
-        ;(format t "~&design: ~a, i: ~a, s: ~a~%" design i (subseq design i))
         (loop for towel in towels
               do (when (cl-ppcre:scan (format nil "^~a" towel)
                                       (subseq design i))
@@ -29,17 +28,19 @@
     (let ((possible 0))
       (loop for design in designs
             do (let ((res (is-possible towels design 0)))
-                 ;(format t "~&design: ~a, res: ~a~%" design res)
                  (when (> res 0)
                    (incf possible))))
       possible)))
 
 (defun part2 (file-name)
-  (let* ((data (parse file-name)))))
+  (multiple-value-bind (towels designs) (parse file-name)
+    (let ((possible 0))
+      (loop for design in designs
+            do (incf possible (is-possible towels design 0)))
+      possible)))
 
-;(time (format t "~&part1: ~a~%" (part1 "input0.txt")))
+(time (format t "~&part1: ~a~%" (part1 "input0.txt")))
 (time (format t "~&part1: ~a~%" (part1 "input1.txt")))
-;; (time (format t "~&part2: ~a~%" (part2 "input0.txt")))
-;; (time (format t "~&part2: ~a~%" (part2 "input1.txt")))
-
+(time (format t "~&part2: ~a~%" (part2 "input0.txt")))
+(time (format t "~&part2: ~a~%" (part2 "input1.txt")))
 
