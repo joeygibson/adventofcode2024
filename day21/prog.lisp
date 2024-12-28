484f2be0a84436f3361a749fc01074f2733efe16
Joey Gibson <joey@joeygibson.com>
2024-12-28 10:07:52 -0500

    day 21, part 1 - a-start, still doesn't work

--- day21/prog.lisp
+++ day21/prog.lisp
@@ -10,32 +10,43 @@
 
 (defun make-numeric-keypad ()
   (let ((keypad (make-hash-table :test #'equal)))
-    (setf (gethash "0" keypad) (cons 1 0))
-    (setf (gethash "A" keypad) (cons 2 0))
-    (setf (gethash "1" keypad) (cons 0 1))
-    (setf (gethash "2" keypad) (cons 1 1))
-    (setf (gethash "3" keypad) (cons 1 2))
-    (setf (gethash "4" keypad) (cons 0 2))
-    (setf (gethash "5" keypad) (cons 1 2))
-    (setf (gethash "6" keypad) (cons 2 2))
-    (setf (gethash "7" keypad) (cons 0 3))
-    (setf (gethash "8" keypad) (cons 1 3))
-    (setf (gethash "9" keypad) (cons 2 3))
+    (setf (gethash (cons 0 0) keypad) "")
+    (setf (gethash (cons 1 0) keypad) "0")
+    (setf (gethash (cons 2 0) keypad) "A")
+    (setf (gethash (cons 0 1) keypad) "1")
+    (setf (gethash (cons 1 1) keypad) "2")
+    (setf (gethash (cons 2 1) keypad) "3")
+    (setf (gethash (cons 0 2) keypad) "4")
+    (setf (gethash (cons 1 2) keypad) "5")
+    (setf (gethash (cons 2 2) keypad) "6")
+    (setf (gethash (cons 0 3) keypad) "7")
+    (setf (gethash (cons 1 3) keypad) "8")
+    (setf (gethash (cons 2 3) keypad) "9")
     keypad))
 
 (defun make-directional-keyboard ()
   (let ((keypad (make-hash-table :test #'equal)))
-    (setf (gethash "<" keypad) (cons 0 0))
-    (setf (gethash "v" keypad) (cons 1 0))
-    (setf (gethash ">" keypad) (cons 2 0))
-    (setf (gethash "^" keypad) (cons 1 1))
-    (setf (gethash "A" keypad) (cons 2 1))
+    (setf (gethash (cons 0 0) keypad) "<")
+    (setf (gethash (cons 1 0) keypad) "v")
+    (setf (gethash (cons 2 0) keypad) ">")
+    (setf (gethash (cons 0 1) keypad) "")
+    (setf (gethash (cons 1 1) keypad) "^")
+    (setf (gethash (cons 2 1) keypad) "A")
     keypad))
 
 (defun parse (file-name)
   (uiop:read-file-lines file-name))
 
-(defun find-moves (start end)
+(defun exclude-gap (key)
+  (equal key ""))
+
+(defun find-key (keypad key)
+  (loop for k being the hash-keys of keypad
+          using (hash-value v)
+        if (equal v key)
+          return k))
+
+(defun convert-moves (start end)
   (cons (- (car end)
            (car start))
         (- (cdr end)
@@ -45,20 +56,30 @@
   (let* ((keys (cons "A" (cl-ppcre:split "" code)))
          (pairs (pairwise keys))
          (all-moves nil))
-    (format t "~&---~%code: ~a -> pairs: ~a~%" code pairs)
+    ;(format t "~&---~%code: ~a -> pairs: ~a~%" code pairs)
     (loop for (a b) in pairs
-          do (let* ((key-a (gethash a keypad))
-                    (key-b (gethash b keypad))
-                    (moves (find-moves key-a key-b))
+          do (let* ((a-key (find-key keypad a))
+                    (b-key (find-key keypad b))
+                    (path (reverse (a-star a-key b-key keypad #'grid-neighbors #'(lambda (a b) 1) #'manhattan-distance #'exclude-gap)))                    
+                    (moves (mapcar (lambda (pair)
+                                     ;(format t "~&pair: ~a~%" pair)
+                                     (convert-moves (car pair) (cadr pair)))
+                                   (pairwise path)))
                     (tmp-move nil))
-               (cond ((< (car moves) 0)
-                      (push (mul-string (abs (car moves)) #\<) tmp-move))
-                     ((> (car moves) 0)
-                      (push (mul-string (abs (car moves)) #\>) tmp-move)))
-               (cond ((< (cdr moves) 0)
-                      (push (mul-string (abs (cdr moves)) #\v) tmp-move))
-                     ((> (cdr moves) 0)
-                      (push (mul-string (abs (cdr moves)) #\^) tmp-move)))
+               ;; (format t "~&a: ~a, b: ~a~%" a b)
+               ;; (format t "~&a-key: ~a, b-key: ~a~%" a-key b-key)
+               ;; (format t "~&path: ~a~%" path)
+               ;(format t "~&moves: ~a~%" moves)
+               (loop for move in moves
+                     do (progn
+                          (cond ((< (car move) 0)
+                                 (push (mul-string (abs (car move)) #\<) tmp-move))
+                                ((> (car move) 0)
+                                 (push (mul-string (abs (car move)) #\>) tmp-move)))
+                          (cond ((< (cdr move) 0)
+                                 (push (mul-string (abs (cdr move)) #\v) tmp-move))
+                                ((> (cdr move) 0)
+                                 (push (mul-string (abs (cdr move)) #\^) tmp-move)))))               
                (push (join "" (reverse tmp-move)) all-moves)))
     (format nil "~aA" (join "A" (reverse all-moves)))))
 
@@ -73,7 +94,8 @@
                     (dir-2-moves (enter-code dir-keypad dir-1-moves))
                     (numeric-portion (parse-integer (remove #\A code)))
                     (complexity (* numeric-portion (length dir-2-moves))))
-               (format t "~&~a~%~t~a~%~t~t~a~%~t~t~tcomplexity: ~a~%"
+               (format t "~&~a~%~a~%~t~a~%~t~t~a~%~t~t~tcomplexity: ~a~%~%"
+                       code
                        code-moves
                        dir-1-moves
                        dir-2-moves
@@ -81,7 +103,7 @@
                (push complexity complexities)))
     (reduce #'+ complexities)))
 
-(time (format t "~&part1: ~a~%" (part1 "input0.txt")))
+(time (format t "~&part1: ~a~%" (part1 "input1.txt")))
 
 
 
